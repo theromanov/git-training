570eb264076d98b805f771908a2aaa17537bdaba
Roman <romansyplyviy@gmail.com>
2026-01-11 15:10:13 +0100

    added my first js-project

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 "use strict";
 
-let a = 5000;
-const b = 4000;
+const numberOfFilms = +prompt("Скільки фільмів ви вже бачили?", "");
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
 
-a = 3355;
+const a = prompt("Назвіть один з останніх переглянутих фільмів", "");
+const b = +prompt("На скільки оціните його?", "");
+const c = prompt("Назвіть один з останніх переглянутих фільмів", "");
+const d = +prompt("На скільки оціните його?", "");
 
-console.log(a);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
